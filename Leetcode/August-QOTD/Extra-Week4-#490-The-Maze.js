aa3ab40b9229752216422f0a686d2bfc28e60add
Mark <58439812+mkzheng972@users.noreply.github.com>
2020-08-23 23:36:49 -0400

    Update Extra-Week4-#490-The-Maze.js

--- Leetcode/August-QOTD/Extra-Week4-#490-The-Maze.js
+++ Leetcode/August-QOTD/Extra-Week4-#490-The-Maze.js
@@ -11,6 +11,7 @@ var hasPath = function(maze, start, destination) {
     once the loop stops
     we have reached an obstacle and must go back one step
     check if current new position was visited or not
+    visited is value of 2
     and dfs into new position
     once you 
     */
@@ -19,24 +20,24 @@ var hasPath = function(maze, start, destination) {
     let m = maze[0].length
     let found = false
     function dfs(position) {
-        if (found) {
+        if (found) {    // return from all call stacks if we already reached destination
             return
         }
-        maze[position[0]][position[1]] = 2
+        maze[position[0]][position[1]] = 2      // set current position to visited
         if (position[0] === destination[0] && position[1] === destination[1]) {
             found = true
             return
         }
-        for (let dir of dirs) {
+        for (let dir of dirs) {     // dfs towards each direction
             let [x1,y1] = position
             let [dx, dy] = dir
             while (x1 >= 0 && x1 < n && y1 >= 0 && y1 < m && maze[x1][y1] !== 1) {
                 x1 += dx
                 y1 += dy
-            }
+            }           // loop towards the end of the direction
             x1 -= dx
             y1 -= dy
-            if (maze[x1][y1] === 0) {
+            if (maze[x1][y1] === 0) {   // new position not visited yet
                 dfs([x1,y1])
             }
         }
