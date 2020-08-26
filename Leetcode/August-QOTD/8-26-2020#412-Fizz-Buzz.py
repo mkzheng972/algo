4e048539b3fa7a3b526342edf8dee99302e9798c
Mark <58439812+mkzheng972@users.noreply.github.com>
2020-08-26 16:54:28 -0400

    Update 8-26-2020#412-Fizz-Buzz.py

--- Leetcode/August-QOTD/8-26-2020#412-Fizz-Buzz.py
+++ Leetcode/August-QOTD/8-26-2020#412-Fizz-Buzz.py
@@ -1,6 +1,25 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         
+        # in case of map[7] = 'Jazz'
+        map = {}
+        map[3] = 'Fizz'
+        map[5] = 'Buzz'
+        output = []
+        for i in range(1, n+1):
+            newStr = ''
+            for key in map:
+                if i % key == 0:
+                    newStr += map[key]
+            if len(newStr) == 0:
+                output.append(str(i))
+            else:
+                output.append(newStr)
+        return output
+    
+        """
+        brute force simple version
+        
         output = []
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
@@ -12,3 +31,4 @@ class Solution:
             else:
                 output.append(str(i))
         return output
+        """
