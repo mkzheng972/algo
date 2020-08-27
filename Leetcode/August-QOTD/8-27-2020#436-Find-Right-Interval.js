885efd1cf94c3d16b8863c04b4821db05523c28e
Mark <58439812+mkzheng972@users.noreply.github.com>
2020-08-27 13:28:37 -0400

    Update 8-27-2020#436-Find-Right-Interval.js

--- Leetcode/August-QOTD/8-27-2020#436-Find-Right-Interval.js
+++ Leetcode/August-QOTD/8-27-2020#436-Find-Right-Interval.js
@@ -30,39 +30,40 @@ var findRightInterval = function(intervals) {
         let [start,end] = intervals[i]
         if (map[start] === undefined) {
             map[start] = i
+            arr.push([start,i])
         }
         highKey = Math.max(highKey, start)
     }
-    
-    // place key,val into an array for binary search later on
-    for (let key in map) {
-        arr.push([parseInt(key), map[key]])
-    }
+
     arr.sort((a,b) => a[0]-b[0])    // sort the array asc order for binary search
     
     // get index for each interval
     for (let j = 0; j < intervals.length; j++) {
         let [start,end] = intervals[j]
         
-        if (map[end] !== undefined) {   // direct end interval match
+        if (map[end] !== undefined) {   // direct end interval match (not necessary)
             output[j] = map[end]
         } else if (end < highKey) {     // higher end interval exists
-        
-            // binary search o(logn)
-            let low = 0
-            let high = arr.length-1
-            while (low < high) {
-                let mid = low + Math.floor((high - low) / 2)
-                if (arr[mid][0] >= end) {
-                    high = mid
-                } else {
-                    low = mid + 1
-                }
-            }
-            output[j] = arr[low][1]
+            let val = binSearch(end)
+            output[j] = arr[val][1]
         } else {       // no higher value key exists
             output[j] = -1
         }
     }
     return output
+    
+    function binSearch(num) {
+        // binary search o(logn)
+        let low = 0
+        let high = arr.length-1
+        while (low < high) {
+            let mid = low + Math.floor((high - low) / 2)
+            if (arr[mid][0] >= num) {
+                high = mid
+            } else {
+                low = mid + 1
+            }
+        }
+        return low
+    }
 };
