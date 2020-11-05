956cccdc3efc7b11bac87354f5bd5855c44311d0
Mark <58439812+mkzheng972@users.noreply.github.com>
2020-11-05 11:54:19 -0500

    Create Binary Search.js

---
+++ Leetcode/Binary Search/Binary Search.js
@@ -0,0 +1,17 @@
+var search = function(nums, target) {
+    
+    let left = 0
+    let right = nums.length-1
+    
+    while (left <= right) {
+        let mid = left + Math.floor((right-left) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            right = mid - 1
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        }
+    }
+    return -1
+};
