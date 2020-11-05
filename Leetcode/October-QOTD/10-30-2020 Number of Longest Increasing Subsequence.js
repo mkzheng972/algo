3595f4aa71c638f1a0bb249a6bbb24426e9d63b6
Mark <58439812+mkzheng972@users.noreply.github.com>
2020-11-05 12:15:47 -0500

    Create 10-30-2020 Number of Longest Increasing Subsequence.js

---
+++ Leetcode/October-QOTD/10-30-2020 Number of Longest Increasing Subsequence.js
@@ -0,0 +1,28 @@
+var findNumberOfLIS = function(nums) {
+    
+    let n = nums.length
+    let len = new Array(n).fill(1)
+    let count = new Array(n).fill(1)
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                if (len[i] <= len[j]) {
+                    len[i] = len[j] + 1
+                    count[i] = count[j]
+                } else if (len[i] === len[j] + 1) {
+                    count[i] += count[j]
+                }
+            }
+        }
+    }
+    
+    let output = 0
+    let longest = Math.max(...len)
+    for (let i = 0; i < n; i++) {
+        if (len[i] === longest) {
+            output += count[i]
+        }
+    }
+    return output
+};
